6b7f015891b881657276800370682b2d1b33c17d
Nikhil3522 <Nikhiljha3522@gmail.com>
2024-12-19 17:53:18 +0530

    podcast category container

--- side_podcast_category.php
+++ side_podcast_category.php
@@ -1,36 +1,36 @@
-<div class="col-lg-3 col-12 order-2 order-lg-1 md-mt-40 sm-mt-40">
+<div class="col-lg-3 col-12 order-2 order-lg-1">
     <div class="shop__sidebar">
         <!-- End Single Widget -->
-        <aside class="widget__categories products--cat" style="margin-top: 40px;">
-            <h3 class="widget__title">Book Categories</h3>
+        <aside class="widget__categories products--cat">
+            <h3 class="widget__title">Podcast Categories</h3>
            
             <ul>
                 <li>
                     <a 
-                        href="category.php?category_id=<?php echo $category_id; ?>" 
+                        href="podcast.php?category_id=<?php echo $category_id; ?>" 
                         style="font-weight: <?= $_GET['category_id'] == $category_id ? 'bold' : 'normal' ?>; color: <?= $_GET['category_id'] == $category_id ? '#ef0029' : 'black' ?>"
                         >
                         All
                     </a>
                 </li>
                 <?php
-                    // $query = "SELECT * FROM categories";
-                    $query = "SELECT A.*, 
-                                (SELECT B.id 
-                                FROM ebook AS B 
-                                WHERE FIND_IN_SET(A.id, B.cat_id) AND B.active = 1
-                                ORDER BY B.id ASC 
-                                LIMIT 1) AS book_id
-                            FROM categories AS A;";
+                    $query = "SELECT * FROM pulse.categories";
+                    // $query = "SELECT A.*, 
+                    //             (SELECT B.id 
+                    //             FROM ebook AS B 
+                    //             WHERE FIND_IN_SET(A.id, B.cat_id) AND B.active = 1
+                    //             ORDER BY B.id ASC 
+                    //             LIMIT 1) AS book_id
+                    //         FROM categories AS A;";
                     $result = $conn->query($query);
                     while($row = $result->fetch_assoc()){
                         $category_id = $row['id'];
                         $category = $row['category'];
-                        $book_id = $row['book_id'];
+                        // $book_id = $row['book_id'];
                 ?>
                     <li>
                         <a 
-                            <?php echo $book_id === NULL ? '' : 'href="category.php?category_id=' . $category_id . '"'; ?>
+                            href="podcast.php?category_id=<?= $category_id; ?>"
                             style="font-weight: <?= $_GET['category_id'] == $category_id ? 'bold' : 'normal' ?>; color: <?= $_GET['category_id'] == $category_id ? '#ef0029' : 'black' ?>">
                             <?php echo $category; ?>
                         </a>
